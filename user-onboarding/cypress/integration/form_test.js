31bedd1c9656e9e30ad57cf341ef0c5a9cdc97dd
Hailey Magoline <magoline.hailey@gmail.com>
2021-12-16 20:55:13 -0600

    formattin fixes in code.

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -1,16 +1,12 @@
 describe('Quotes App', () => {
 
     beforeEach(() => {
-        // Each test needs fresh state!
-        // Never rely on state left over from previous tests
-        // Every test should work in isolation (MUST)
+     
         cy.visit('http://localhost:3000')
 
     }) 
     
     it('Sanity Check', () => {
-        // 'it' is a test
-        // expect is an assertion
         expect(2 + 2).to.equal(4);
         expect(2 + 2).not.to.equal(2);
         expect({}).to.eql({}); 
@@ -91,7 +87,6 @@ describe('Quotes App', () => {
 
         fNameInput().type('firstName')
           submitBtn().should('be.disabled')
-
         
           lNameInput()
           .type('lastName')
